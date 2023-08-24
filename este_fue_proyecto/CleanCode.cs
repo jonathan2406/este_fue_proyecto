441dfa14ec1dcc49ea7b1904fb538a40fd7e2e48
David Lasso <davidlh2005@gmail.com>
2023-08-23 23:16:43 -0500

    Cambios en menú

--- este_fue_proyecto/CleanCode.cs
+++ este_fue_proyecto/CleanCode.cs
@@ -68,7 +68,7 @@ namespace este_fue_proyecto
                     }
                     else
                     {
-                        Console.Write("vacio! ");
+                        Console.Write("Vacío! ");
                     }
                 }
                 Console.WriteLine();
@@ -77,22 +77,25 @@ namespace este_fue_proyecto
         }
         public void menu(Casa casa, Habitante persona_solicitante)
         {
+            Console.WriteLine("Plano de la casa actual: ");
+            Console.WriteLine("-------------------------------------------------------------------------------------");
             matriz_vieja_trabajador = casa.get_plano();
             MostrarMatriz_trabajador();
             int costo_trabajo = 0;
             double acumulador_horas = 0;
-            Console.WriteLine("Bienvenido a intervenciones clean code");
-            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------------------");
+            Console.WriteLine("---- Bienvenido a intervenciones CleanCode ----");
+            Console.WriteLine("-------------------------------------------------------------------------------------");
 
-            primer_while:
+        primer_while:
             while (true)
             {
-                Console.WriteLine("Que servicio quiere realizar? \n ");
-                Console.WriteLine("presione 1 para contruir una habitacion............" +
-                    "\npresione 2 para ampliar una habitacion............" +
-                    "\npresione 3 para decorar habitacion............" +
-                    "\npresione 4 para reparar habitacion............" +
-                    "\npresione 5 para salir----------------\n");
+                Console.WriteLine("-- ¿Qué servicio quiere realizar? \n ");
+                Console.WriteLine("-- Presione 1 para construir una habitación." +
+                    "\n-- Presione 2 para ampliar una habitación." +
+                    "\n-- Presione 3 para decorar habitación." +
+                    "\n-- Presione 4 para reparar habitación.." +
+                    "\n-- Presione 5 para salir.");
                 string decision = Console.ReadLine();
                 if (int.TryParse(decision, out int numero_decision) && numero_decision == 1 || numero_decision == 2 || numero_decision == 3 || numero_decision == 4 || numero_decision == 5)
                 {
@@ -103,30 +106,34 @@ namespace este_fue_proyecto
                 }
                 else
                 {
-                    Console.WriteLine("ingrese un numero valido porfavor");
+                    Console.WriteLine("Ingrese un número valido por favor");
                     goto primer_while;
                 }
                 if (numero_decision == 1)
                 {
-                    Console.WriteLine("nombre que desea poner a la habitacion:       ");
+                    Console.WriteLine("-- Nombre que desea poner a la habitación:       ");
                     string nombre_habitacion = Console.ReadLine();
-                    Console.WriteLine("\ningrese el numero de la fila donde desea poner:    ");
+                    Console.WriteLine($"\n-- Ingrese el número de la fila donde desea poner: {nombre_habitacion}   ");
                     int fila = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de columna donde desea poner:    ");
+                    Console.WriteLine($"\n-- Ingrese el número de columna donde desea poner: {nombre_habitacion}   ");
                     int columna = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de metros que desea la habitacion:     ");
+                    Console.WriteLine($"\n-- Ingrese el número de metros que desea la habitación: {nombre_habitacion}   ");
                     int metros = int.Parse(Console.ReadLine());
 
                     Habitacion nueva_habitacion = casa.añadir_habitacion(nombre_habitacion, fila, columna, metros);
-                    Console.WriteLine("se contruyo");
+                    Console.WriteLine("-------------------------------------------------------------------------------------");
+                    Console.WriteLine($"-- Se construyo: {nueva_habitacion.Nombre}");
+                    Console.WriteLine("\n-- Plano de la casa: ");
+                    Console.WriteLine("-------------------------------------------------------------------------------------");
                     MostrarMatriz_trabajador();
+                    Console.WriteLine("-------------------------------------------------------------------------------------");
                     if (nueva_habitacion != null)
                     {
                         persona_solicitante.HabitacionAsignada = nueva_habitacion;
                     }
                     else
                     {
-                        Console.WriteLine("no se puede hacer eso........");
+                        Console.WriteLine("-- No se puede hacer eso........");
                         goto primer_while;
                     }
 
@@ -138,16 +145,16 @@ namespace este_fue_proyecto
                     tiempoTotal += trabajadores * horasPorMetroCuadrado;
                     double costoTotal = tiempoTotal * 40000;
                     List<Remodelador> remodeladores_para_construir = new List<Remodelador> { };
-                    Console.WriteLine($"el valor de remodeladores despues de crearse es: {remodeladores_para_construir.Count}");
+                    Console.WriteLine($"-- El valor de remodeladores despues de crearse es: {remodeladores_para_construir.Count}");
                     for (int i = 0; i < lista_remodeladores.Count; i++)
                     {
                         Remodelador resultado = coger_remodelador_desocupado();
-                        Console.WriteLine($"el resultado de desocupado {resultado}");
+                        Console.WriteLine($"-- El resultado de desocupado es: {resultado}");
                         if (resultado == null)
                         {
                             casa.set_matriz(matriz_vieja_trabajador);
                             casa.set_matriz_vieja(matriz_vieja_trabajador);
-                            Console.WriteLine("nos faltan trabajadores para hacer esa accion, muchos ocupados ya");
+                            Console.WriteLine(" -- Nos faltan trabajadores para hacer esa acción, muchos ocupados ya");
                             goto primer_while;
                         }
                         else
@@ -155,17 +162,17 @@ namespace este_fue_proyecto
                             remodeladores_para_construir.Add(resultado);
                         }
                     }
-                    Console.WriteLine($"el valor de remodeladores listos es: {remodeladores_para_construir.Count}");
-                    Console.WriteLine($"el valor de trabajadores es: {trabajadores}");
+                    Console.WriteLine($"-- El valor de remodeladores listos es: {remodeladores_para_construir.Count}");
+                    Console.WriteLine($"-- El valor de trabajadores es: {trabajadores}");
                     if (remodeladores_para_construir.Count <= trabajadores)
                     {
-                        Console.WriteLine("tenemos todos los trabajadores para hacer esta peticion!!!!!");
+                        Console.WriteLine("-- ¡¡¡¡Tenemos todos los trabajadores para hacer esta petición!!!!");
                     }
                     else
                     {
                         casa.set_matriz(matriz_vieja_trabajador);
                         casa.set_matriz_vieja(matriz_vieja_trabajador);
-                        Console.WriteLine("nos faltan trabajadores para hacer esa accion, muchos ocupados ya");
+                        Console.WriteLine("-- Nos faltan trabajadores para hacer esa acción, muchos ocupados ya");
                         goto primer_while;
 
                     }
@@ -176,12 +183,11 @@ namespace este_fue_proyecto
                     int tiempo_simulacion = (metros/10) * 5;
                     foreach (Remodelador iteracion in remodeladores_para_construir)
                     {
-                        Console.WriteLine($"la hora de antes de sumar es {iteracion.hora_ocupacion}");
+                        Console.WriteLine($"-- La hora antes de sumarse es: {iteracion.hora_ocupacion}");
                         iteracion.hora_ocupacion = iteracion.hora_ocupacion.AddSeconds(tiempo_simulacion);
-                        Console.WriteLine($"la hora de despues de sumar es {iteracion.hora_ocupacion}");
+                        Console.WriteLine($"-- La hora despues de sumarse es: {iteracion.hora_ocupacion}");
                     }
-                    Console.WriteLine("fin interaccionnnnnnnnnnnnnnnnnnnnnnnnnn");
-                    
+                    Console.WriteLine("Finalizó la iteración.");
 
                     casa.MostrarMatriz();
 
@@ -190,26 +196,26 @@ namespace este_fue_proyecto
                 }
                 else if (numero_decision == 2)
                 {
-                    Console.WriteLine("\ningrese el numero de la fila donde esta hubicada la habitacion:    ");
+                    Console.WriteLine("\n-- Ingrese el número de la fila donde está ubicada la habitación:    ");
                     int fila = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de columna donde esta hubicada la habitacion:    ");
+                    Console.WriteLine("\n-- Ingrese el número de columna donde está ubicada la habitación:    ");
                     int columna = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de metros que desea ampliar:     ");
+                    Console.WriteLine("\n-- ingrese el numero de metros que desea ampliar:     ");
                     int metros = int.Parse(Console.ReadLine());
 
                     if (casa.ampliar_habitacion(casa.get_plano()[fila, columna], metros) == false)
                     {
-                        Console.WriteLine("no se puede hacer esta accion .......");
+                        Console.WriteLine("-- No se puede hacer esta acción .......");
                         goto primer_while;
                     }
                 }
                 else if (numero_decision == 3)
                 {
-                    Console.WriteLine("Seleccione la habitación que desea decorar:");
+                    Console.WriteLine("-- Seleccione la habitación que desea decorar:");
                     casa.MostrarMatriz();
-                    Console.WriteLine("Ingrese el número de la fila:");
+                    Console.WriteLine("-- Ingrese el número de la fila:");
                     int fila = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Ingrese el número de la columna:");
+                    Console.WriteLine("-- Ingrese el número de la columna:");
                     int columna = int.Parse(Console.ReadLine());
 
                     Remodelador remodelador = coger_remodelador_desocupado();
@@ -223,15 +229,15 @@ namespace este_fue_proyecto
                     }
                     else
                     {
-                        Console.WriteLine("No hay remodeladores disponibles en este momento.");
+                        Console.WriteLine("-- No hay remodeladores disponibles en este momento.");
                     }
 
                 }
                 else if (numero_decision == 4)
                 {
-                    Console.WriteLine("\ningrese el numero de la fila donde está ubicada la habitación:    ");
+                    Console.WriteLine("\n-- Ingrese el número de la fila donde está ubicada la habitación:    ");
                     int fila = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de columna donde está ubicada la habitación:    ");
+                    Console.WriteLine("\n-- Ingrese el numero de columna donde está ubicada la habitación:    ");
                     int columna = int.Parse(Console.ReadLine());
 
                     Habitacion habitacionAReparar = casa.get_plano()[fila, columna];

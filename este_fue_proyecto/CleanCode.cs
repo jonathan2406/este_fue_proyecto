ad050de3a96d5a1af8484944f9537160c4bf3b9c
jonathan2406 <jonathan2406.garcia@gmail.com>
2023-08-23 23:18:29 -0500

    ya

--- este_fue_proyecto/CleanCode.cs
+++ este_fue_proyecto/CleanCode.cs
@@ -11,8 +11,6 @@ namespace este_fue_proyecto
 {
     internal class CleanCode
     {
-        Habitacion[,] matriz_vieja_trabajador;
-        Habitacion[,] backup_matriz;
         protected List<Remodelador> lista_remodeladores = new List<Remodelador> ();
         int numero_trabajadores;
         
@@ -31,7 +29,6 @@ namespace este_fue_proyecto
             for (int i = 0; i < numero_trabajadores; i++)
             {
                 Remodelador remodelador_i = new Remodelador($"remodelador_{i}");
-                remodelador_i.hora_ocupacion = DateTime.Now;
                 meter_lista_trabajadores(remodelador_i);
             }
         }
@@ -39,175 +36,29 @@ namespace este_fue_proyecto
         {
             return lista_remodeladores;
         }
-        
-        Remodelador coger_remodelador_desocupado()
+
+        public void imprimir_lista_trabajadores()
         {
             foreach (Remodelador iteracion in lista_remodeladores)
             {
-                if (DateTime.Compare(iteracion.hora_ocupacion,DateTime.Now) <  0)
-                {
-                    return iteracion;
-                }
-                else
-                {
-                    return null;
-                }
+                Console.WriteLine($"remodelador imprimir = {iteracion.Nombre}");
             }
-            return null;
         }
-
-        public void MostrarMatriz_trabajador()
+        
+        public List<Remodelador> coger_remodelador_desocupado()
         {
-            for (int i = 0; i < matriz_vieja_trabajador.GetLength(0); i++)
+            List<Remodelador> lista_desocupados = new List<Remodelador> { };
+            for (int i = 0; i < numero_trabajadores; i++)
             {
-                for (int j = 0; j < matriz_vieja_trabajador.GetLength(1); j++)
+                if (lista_remodeladores[i].verificar_disponibilidad_trabajador() == true)
                 {
-                    if (matriz_vieja_trabajador[i, j] != null)
-                    {
-                        Console.Write(matriz_vieja_trabajador[i, j].Nombre + " ");
-                    }
-                    else
-                    {
-                        Console.Write("vacio! ");
-                    }
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-        }
-        public void menu(Casa casa, Habitante persona_solicitante)
-        {
-            matriz_vieja_trabajador = casa.get_plano();
-            MostrarMatriz_trabajador();
-            int costo_trabajo = 0;
-            double acumulador_horas = 0;
-            Console.WriteLine("Bienvenido a intervenciones clean code");
-            Console.WriteLine("--------------------------------------------");
-
-            primer_while:
-            while (true)
-            {
-                Console.WriteLine("Que servicio quiere realizar? \n ");
-                Console.WriteLine("presione 1 para contruir una habitacion............" +
-                    "\npresione 2 para ampliar una habitacion............" +
-                    "\npresione 3 para decorar habitacion............" +
-                    "\npresione 4 para reparar habitacion............" +
-                    "\npresione 5 para salir----------------\n");
-                string decision = Console.ReadLine();
-                if (int.TryParse(decision, out int numero_decision) && numero_decision == 1 || numero_decision == 2 || numero_decision == 3 || numero_decision == 4 || numero_decision == 5)
-                {
-                    if (numero_decision == 5)
-                    {
-                        return;
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("ingrese un numero valido porfavor");
-                    goto primer_while;
-                }
-                if (numero_decision == 1)
-                {
-                    Console.WriteLine("nombre que desea poner a la habitacion:       ");
-                    string nombre_habitacion = Console.ReadLine();
-                    Console.WriteLine("\ningrese el numero de la fila donde desea poner:    ");
-                    int fila = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de columna donde desea poner:    ");
-                    int columna = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de metros que desea la habitacion:     ");
-                    int metros = int.Parse(Console.ReadLine());
-
-                    Habitacion nueva_habitacion = casa.añadir_habitacion(nombre_habitacion, fila, columna, metros);
-                    Console.WriteLine("se contruyo");
-                    MostrarMatriz_trabajador();
-                    if (nueva_habitacion != null)
-                    {
-                        persona_solicitante.HabitacionAsignada = nueva_habitacion;
-                    }
-                    else
-                    {
-                        Console.WriteLine("no se puede hacer eso........");
-                        goto primer_while;
-                    }
-
-                    //calculo costo y numero de trabajadores
-                    double horasPorMetroCuadrado = 1.5;
-                    double metrosCuadrados = metros;
-                    double tiempoTotal = metrosCuadrados * horasPorMetroCuadrado;
-                    int trabajadores = ((int)metrosCuadrados / 10) * 2;
-                    tiempoTotal += trabajadores * horasPorMetroCuadrado;
-                    double costoTotal = tiempoTotal * 40000;
-                    List<Remodelador> remodeladores_para_construir = new List<Remodelador> { };
-                    Console.WriteLine($"el valor de remodeladores despues de crearse es: {remodeladores_para_construir.Count}");
-                    for (int i = 0; i < lista_remodeladores.Count; i++)
-                    {
-                        Remodelador resultado = coger_remodelador_desocupado();
-                        Console.WriteLine($"el resultado de desocupado {resultado}");
-                        if (resultado == null)
-                        {
-                            casa.set_matriz(matriz_vieja_trabajador);
-                            casa.set_matriz_vieja(matriz_vieja_trabajador);
-                            Console.WriteLine("nos faltan trabajadores para hacer esa accion, muchos ocupados ya");
-                            goto primer_while;
-                        }
-                        else
-                        {
-                            remodeladores_para_construir.Add(resultado);
-                        }
-                    }
-                    Console.WriteLine($"el valor de remodeladores listos es: {remodeladores_para_construir.Count}");
-                    Console.WriteLine($"el valor de trabajadores es: {trabajadores}");
-                    if (remodeladores_para_construir.Count <= trabajadores)
-                    {
-                        Console.WriteLine("tenemos todos los trabajadores para hacer esta peticion!!!!!");
-                    }
-                    else
-                    {
-                        casa.set_matriz(matriz_vieja_trabajador);
-                        casa.set_matriz_vieja(matriz_vieja_trabajador);
-                        Console.WriteLine("nos faltan trabajadores para hacer esa accion, muchos ocupados ya");
-                        goto primer_while;
-
-                    }
-                    acumulador_horas += tiempoTotal;
-
-                    // vamos a poner un tiempo ficticcio para que los trabajadores se puedan utilizar despues
-
-                    int tiempo_simulacion = (metros/10) * 5;
-                    foreach (Remodelador iteracion in remodeladores_para_construir)
-                    {
-                        Console.WriteLine($"la hora de antes de sumar es {iteracion.hora_ocupacion}");
-                        iteracion.hora_ocupacion = iteracion.hora_ocupacion.AddSeconds(tiempo_simulacion);
-                        Console.WriteLine($"la hora de despues de sumar es {iteracion.hora_ocupacion}");
-                    }
-                    Console.WriteLine("fin interaccionnnnnnnnnnnnnnnnnnnnnnnnnn");
-                    
-
-                    casa.MostrarMatriz();
-
-
-
-                }
-                else if (numero_decision == 2)
-                {
-                    Console.WriteLine("\ningrese el numero de la fila donde esta hubicada la habitacion:    ");
-                    int fila = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de columna donde esta hubicada la habitacion:    ");
-                    int columna = int.Parse(Console.ReadLine());
-                    Console.WriteLine("\ningrese el numero de metros que desea ampliar:     ");
-                    int metros = int.Parse(Console.ReadLine());
-
-                    if (casa.ampliar_habitacion(casa.get_plano()[fila, columna], metros) == false)
-                    {
-                        Console.WriteLine("no se puede hacer esta accion .......");
-                        goto primer_while;
-                    }
-                }
-                else if (numero_decision == 3)
-                {
-
+                    lista_desocupados.Add(lista_remodeladores[i]);
                 }
             }
+            return lista_desocupados;
         }
+
+
+
     }
 }
